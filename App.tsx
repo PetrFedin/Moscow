cb3bc965bce41ac6e18bae287767bec912c86d59
Petr Fedin <81327591+PetrFedin@users.noreply.github.com>
2026-09-16 02:13:00 +0300

    Add gesture root for unified interaction physics

--- App.tsx
+++ App.tsx
@@ -1,3 +1,10 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MoscowDemoShell from './src/MoscowDemoShell';
 
-export default MoscowDemoShell;
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <MoscowDemoShell />
+    </GestureHandlerRootView>
+  );
+}
