62db27729de411537ea52fab9dedc8203e3bf759
Petr Fedin <81327591+PetrFedin@users.noreply.github.com>
2026-09-26 02:01:26 +0300

    MFW: refresh offline cache v6

--- mfw/sw.js
+++ mfw/sw.js
@@ -1,5 +1,5 @@
-const CACHE='mfw-demo-v5';
-const ASSETS=['/','/index.html','/styles.css?v=5','/vendor/jsQR.js?v=1','/app.js?v=5','/manifest.webmanifest','/icon.svg'];
+const CACHE='mfw-demo-v6';
+const ASSETS=['/','/index.html','/styles.css?v=6','/vendor/jsQR.js?v=1','/app.js?v=6','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
   self.skipWaiting();
