34d2424438cfedc172d2807066a5095f13d33fea
Petr Fedin <81327591+PetrFedin@users.noreply.github.com>
2026-09-26 10:20:17 +0300

    MFW: refresh visual production offline cache v9

--- mfw/sw.js
+++ mfw/sw.js
@@ -1,5 +1,5 @@
-const CACHE='mfw-demo-v6';
-const ASSETS=['/','/index.html','/styles.css?v=6','/vendor/jsQR.js?v=1','/app.js?v=6','/manifest.webmanifest','/icon.svg'];
+const CACHE='mfw-demo-v9';
+const ASSETS=['/','/index.html','/styles.css?v=9','/vendor/jsQR.js?v=1','/app.js?v=9','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
   self.skipWaiting();
